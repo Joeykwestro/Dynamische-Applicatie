115fb7967637b4768c8afa56f11c3004aa406cc0
Joey <99062903@mydavinci.nl>
2021-03-23 11:07:01 +0100

    html af, table af, results in table.

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 	require('functions/connection.php');
-?>
 
+	$query = 'SELECT name, health, attack, defense FROM characters';
+	$sth = $dbh->prepare($query);
+	$sth->execute();
+	$result = $sth->fetchAll();
+?>
 
 
 <!DOCTYPE html>
@@ -15,11 +19,28 @@
 		<div id='header'>
 			<img id='headerimg' src="images/header.jpg">
 		</div>
-		<div id='menu'>
-			
-		</div>
 		<div id='content'>
-			
+			<table id='tabel'>
+				<tr>
+					<th>Name</th>
+					<th>Health</th>
+					<th>Attack</th>
+					<th>Defense</th>
+				</tr>
+				<?php 
+					foreach($result as $rows){
+				?>
+				<tr>
+					<td><?php echo $rows[0]; ?></td>
+					<td><?php echo $rows[1]; ?></td>
+					<td><?php echo $rows[2]; ?></td>
+					<td><?php echo $rows[3]; ?></td>
+				</tr>
+				<?php 
+					}
+				 ?>
+
+			</table>
 		</div>
 		<div id='footer'>
 			<footer>©Joey Kwestro</footer>
