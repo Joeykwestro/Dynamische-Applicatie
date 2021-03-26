8e627e0d59b6de1c5b49d9c6038b1de9ecee5d37
Joey <99062903@mydavinci.nl>
2021-03-26 09:22:33 +0100

    .

--- index.php
+++ index.php
@@ -1,76 +1,66 @@
 <?php
+
+	$value = true;
 	require('functions/connection.php');
 
-	$query = 'SELECT name, health, attack, defense FROM characters ORDER BY name';
-	$sth = $dbh->prepare($query);
-	$sth->execute();
-	$result = $sth->fetchAll();
+	if($value == true){
+		$query = 'SELECT name, health, attack, defense, avatar, bio FROM characters ORDER BY name';
+		$sth = $dbh->prepare($query);
+		$sth->execute();
+		$result = $sth->fetchAll();
 
-	$query2 = 'SELECT COUNT(name) FROM characters';
-	$sth2 = $dbh->prepare($query2);
-	$sth2->execute();
-	$qty = $sth2->fetch();
+		$query2 = 'SELECT COUNT(name) FROM characters';
+		$sth2 = $dbh->prepare($query2);
+		$sth2->execute();
+		$qty = $sth2->fetch();
+	}
 
 	$count = 0;
 	$check = false;
+
+
+	if($value == true){
 ?>
 
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Dynamische Website</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>All Characters</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
-	<div id='allcontainer'>
-		<div id='header'>
-			<img id='headerimg' src="images/header.jpg">
-		</div>
-		<div id='content'>
-			<?php
-				if(!$check){
-			?>
-			<table id='tabel'>
-				<tr>
-					<th>Name (<?php echo $qty[0];?>)</th>
-					<th>Health</th>
-					<th>Attack</th>
-					<th>Defense</th>
-					<th>Link</th>
-				</tr>
-				<?php 
-					foreach($result as $rows){
-						$count++;
-				?>
-				<tr>
-					<td><?php echo $rows[0]; ?></td>
-					<td><?php echo $rows[1]; ?></td>
-					<td><?php echo $rows[2]; ?></td>
-					<td><?php echo $rows[3]; ?></td>
-					<td><li><a href="pages/character.php">View</a></li></td>
-				</tr>
-				<?php 
-					}
-				?>
-			</table>
-			
-			
-			<?php 
-				}
-				if($check == true){
-					var_dump($check);
-			?>
-
-			<h2>gelukt</h2>
+	<header><h1>Alle <?php echo $qty[0];?> characters uit de database</h1></header>
+	<div id="container">
+		<?php 
+			foreach ($result as $rows) {
 				
-			<?php 
-				}
-			?>
-		</div>
-		<div id='footer'>
-			<footer>©Joey Kwestro</footer>
-		</div>
+		?>
+	    <a class="item" id='<?php echo $count;?>' href="pages/character.php">
+	        <div class="left">
+	            <img class="avatar" src="images/<?php echo $result[$count]['avatar'];?>">
+	        </div>
+	        <div class="right">
+	            <h2><?php echo $result[$count]['name']; ?></h2>
+	            <div class="stats">
+	                <ul class="fa-ul">
+	                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?php echo $result[$count]['health']; ?></li>
+	                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?php echo $result[$count]['attack']; ?></li>
+	                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?php echo $result[$count]['defense']; ?></li>
+	                </ul>
+	            </div>
+	        </div>
+	        <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
+	    </a>
+	    <?php 
+	    	$count++;
+	    }
+	    ?>
 	</div>
+	<footer>&copy; Joey Kwestro 2021</footer>
 </body>
 </html>
+
+<?php } ?>
--- pages/character.php
+++ pages/character.php
@@ -1,22 +1,68 @@
+<?php  
+    require('../functions/connection.php');
+
+    $value = false;
+
+    include('../index.php')
+
+    $query = 'SELECT name, health, attack, defense, avatar, bio FROM characters ORDER BY name';
+    $sth = $dbh->prepare($query);
+    $sth->execute();
+    $result = $sth->fetchAll();
+
+    $count = 0;
+    $check = false;
+
+?>
+
 
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Character</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
+    <meta charset="UTF-8">
+    <title>Character - Bowser</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="../css/style.css" rel="stylesheet"/>
 </head>
 <body>
-<div id='allcontainer'>
-		<div id='header'>
-			<img id='headerimg' src="../images/header.jpg">
-		</div>
-		<div id='content'>
-
-		</div>
-		<div id='footer'>
-			<footer>©Joey Kwestro</footer>
-		</div>
-	</div>
+<header><h1><?php echo $result[$count]['name']; ?></h1>
+    <a class="backbutton" href="../index.php"><i class="fas fa-long-arrow-alt-left"></i> Terug</a></header>
+<div id="container">
+    <div class="detail">
+        <div class="left">
+            <img class="avatar" src="../images/bowser.jpg">
+            <div class="stats" style="background-color: yellowgreen">
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> 10000</li>
+                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> 400</li>
+                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> 100</li>
+                </ul>
+                <ul class="gear">
+                    <li><b>Weapon</b>: Fire Breath</li>
+                    <li><b>Armor</b>: Giant Shell</li>
+                </ul>
+            </div>
+        </div>
+        <div class="right">
+            <p>
+                Bowser or King Koopa, is a fictional character and the main antagonist of Nintendo's Mario franchise. In
+                Japan, the character bears the title of Great Demon King. In the U.S., the character was first referred
+                to as "Bowser, King of the Koopas" and "the sorcerer king" in the instruction manual.<br/>
+                <br/>
+                Bowser is the leader of the turtle-like Koopa race, and has been the archenemy of Mario since his first
+                appearance, in the 1985 video game Super Mario Bros.<br/>
+                <br/>
+                His ultimate goals are to kidnap Princess Peach, defeat Mario, and conquer the Mushroom Kingdom. Since
+                his debut, he has appeared in almost every Mario franchise game, usually serving as the main antagonist.
+                Bowser is voiced by Kenny James.
+            </p>
+        </div>
+        <div style="clear: both"></div>
+    </div>
+</div>
+<footer>&copy; Joey Kwestro 2020</footer>
 </body>
 </html>
+
+<?php } ?>
